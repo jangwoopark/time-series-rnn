3c0d9587b639cfe7533c7b9a24afa835f609738b
Jang Woo Park <jangpark@g.ucla.edu>
2017-12-28 12:02:02 -0800

    upload files
    new folder

---
+++ my_functions.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+import keras
+
+
+# fill out the function below that transforms the input series 
+# and window-size into a set of input/output pairs for use with our RNN model
+def window_transform_series(series, window_size):
+    # containers for input/output pairs
+    X = []
+    y = []
+
+    for i in range (len(series) - window_size):
+        a = series[i:(i+window_size)]
+        X.append(a)
+        y.append(series[i+window_size])
+
+    # reshape each 
+    X = np.asarray(X)
+    X.shape = (np.shape(X)[0:2])
+    y = np.asarray(y)
+    y.shape = (len(y),1)
+
+    return X,y
+
+# build an RNN to perform regression on our time series input/output data
+def build_part1_RNN(window_size):
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+    return model
